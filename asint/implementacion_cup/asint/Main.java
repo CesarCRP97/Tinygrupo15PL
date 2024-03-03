4ca4b431bf0b89b619c3e910930f1d217988fcd5
Miguel Verdaguer Velázquez <verdx@riseup.net>
2024-03-03 23:09:47 +0100

    Quitados cambios en el main

--- asint/implementacion_cup/asint/Main.java
+++ asint/implementacion_cup/asint/Main.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
+import alex.AnalizadorLexicoTiny;
 //String[] asd= { "-destdir", "src/c_ast_ascendente","-parser" ,"ContructorAst" ,"-symbols", "ClaseLexica","ConstructorAST.cup"};
 //java_cup.Main.main(asd);
 //java -jar cup.jar -parser AnalizadorSintacticoTiny -symbols ClaseLexica Tiny.cup
@@ -11,8 +12,9 @@ public class Main {
 
 	public static void main(String[] args)throws Exception  {
 		// TODO Auto-generated method stub
-		Reader input = new InputStreamReader(new FileInputStream("input.txt"));
-		AnalizadorSintacticoTiny aS=new AnalizadorSintacticoTiny(input);
+		Reader input = new InputStreamReader(new FileInputStream(args[0]));
+		AnalizadorLexicoTiny al=new AnalizadorLexicoTiny(input);
+		AnalizadorSintacticoTiny aS=new AnalizadorSintacticoTiny(al);
 		aS.parse();
 	}
 
