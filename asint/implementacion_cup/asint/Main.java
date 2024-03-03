7b370332d04cb327544e20e5479f5f42b14664a4
Miguel Verdaguer Velázquez <verdx@riseup.net>
2024-03-03 22:49:03 +0100

    Quitado analizador lexico del main

--- asint/implementacion_cup/asint/Main.java
+++ asint/implementacion_cup/asint/Main.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-import alex.AnalizadorLexicoTiny;
 //String[] asd= { "-destdir", "src/c_ast_ascendente","-parser" ,"ContructorAst" ,"-symbols", "ClaseLexica","ConstructorAST.cup"};
 //java_cup.Main.main(asd);
 //java -jar cup.jar -parser AnalizadorSintacticoTiny -symbols ClaseLexica Tiny.cup
@@ -13,8 +12,7 @@ public class Main {
 	public static void main(String[] args)throws Exception  {
 		// TODO Auto-generated method stub
 		Reader input = new InputStreamReader(new FileInputStream("input.txt"));
-		AnalizadorLexicoTiny al=new AnalizadorLexicoTiny(input);
-		AnalizadorSintacticoTiny aS=new AnalizadorSintacticoTiny(al);
+		AnalizadorSintacticoTiny aS=new AnalizadorSintacticoTiny(input);
 		aS.parse();
 	}
 
