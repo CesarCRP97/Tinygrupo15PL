09f98504f25c9263e0775ca1b5c45577e47d8d03
Miguel Verdaguer Velázquez <verdx@riseup.net>
2024-03-03 22:15:34 +0100

    Cambios de Fumu

--- asint/implementacion_cup/asint/Main.java
+++ asint/implementacion_cup/asint/Main.java
@@ -3,15 +3,19 @@ package asint;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import alex.AnalizadorLexicoEval;
 
+import alex.AnalizadorLexicoTiny;
+//String[] asd= { "-destdir", "src/c_ast_ascendente","-parser" ,"ContructorAst" ,"-symbols", "ClaseLexica","ConstructorAST.cup"};
+//java_cup.Main.main(asd);
+//java -jar cup.jar -parser AnalizadorSintacticoTiny -symbols ClaseLexica Tiny.cup
 public class Main {
-   public static void main(String[] args) throws Exception {
-     Reader input = new InputStreamReader(new FileInputStream(args[0]));
-	 AnalizadorLexicoEval alex = new AnalizadorLexicoEval(input);
-	 AnalizadorSintacticoEval asint = new AnalizadorSintacticoEval(alex);
-	 //asint.setScanner(alex);
-	 asint.parse();
- }
-}   
-   
+
+	public static void main(String[] args)throws Exception  {
+		// TODO Auto-generated method stub
+		Reader input = new InputStreamReader(new FileInputStream("input.txt"));
+		AnalizadorLexicoTiny al=new AnalizadorLexicoTiny(input);
+		AnalizadorSintacticoTiny aS=new AnalizadorSintacticoTiny(al);
+		aS.parse();
+	}
+
+}  
