302f2a10562691378ad331a4d5405c4e7009973e
Miguel Verdaguer Velázquez <verdx@riseup.net>
2024-04-10 17:13:31 +0200

    Arreglado main

--- fase3/procesamientos/Main.java
+++ fase3/procesamientos/Main.java
@@ -19,15 +19,16 @@ public class Main {
         String tipo = args[2];
 
         //Inicializamos el arbol de sintaxis abstracta que vayamos a usar
-        if(tipo.equals("asc") {
+        Prog prog_asc = null, prog_desc = null;
+        if(tipo.equals("asc")) {
             Reader input = new InputStreamReader(new FileInputStream(file));
             AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
             c_ast_ascendente.ConstructorASTTiny asint_asc = new c_ast_ascendente.ConstructorASTTiny(alex);
-            Prog prog_asc = (Prog)asint_asc.parse().value;
+            prog_asc = (Prog)asint_asc.parse().value;
         } else if (tipo.equals("desc")) {
             c_ast_descendente.ConstructorASTsTiny asint_desc = new c_ast_descendente.ConstructorASTsTiny(new FileReader(file));
             asint_desc.disable_tracing();
-            Prog prog_desc = asint_desc.analiza();
+            prog_desc = asint_desc.analiza();
         } else {
             System.err.println("Uso: java -jar Procesamientos.jar <fichero> <rec|inter|vis> <asc|desc>");
             System.exit(1);
@@ -40,7 +41,7 @@ public class Main {
             else if (tipo.equals("desc")) {
                 System.out.println(new Procesamiento_rec().imprime(prog_desc));
             }
-        } else if(proc.equals("inter") {
+        } else if(proc.equals("inter")) {
             if(tipo.equals("asc")) {
                 prog_asc.imprime();
             }
