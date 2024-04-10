637d160b5da31cbb4b85b6993ff5769cc3a6b911
Miguel Verdaguer Velázquez <verdx@riseup.net>
2024-04-10 17:09:24 +0200

    Modificado el main para coger todos los procesamientos

--- fase3/procesamientos/Main.java
+++ fase3/procesamientos/Main.java
@@ -9,18 +9,44 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 
 public class Main {
-   public static void main(String[] args) throws Exception {
-         if(args[0].equals("asc")) {
-           Reader input = new InputStreamReader(new FileInputStream(args[1]));
-  	   AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
-	   c_ast_ascendente.ConstructorASTTiny asint = new c_ast_ascendente.ConstructorASTTiny(alex);
-           Prog prog = (Prog)asint.parse().value;
-  	   System.out.println(new Procesamiento_rec().imprime(prog));
-         }
-         else {
-             ConstructorASTsTiny asint = new ConstructorASTsTiny(new FileReader(args[1]));
-             asint.disable_tracing();
-             System.out.println(new Procesamiento_rec().imprime(asint.analiza()));
-         }
+    public static void main(String[] args) throws Exception {
+        if(args.length != 2) {
+            System.err.println("Uso: java -jar Procesamientos.jar <fichero> <rec|inter|vis> <asc|desc>");
+            System.exit(1);
+        }
+        String file = args[0];
+        String proc = args[1];
+        String tipo = args[2];
+
+        //Inicializamos el arbol de sintaxis abstracta que vayamos a usar
+        if(tipo.equals("asc") {
+            Reader input = new InputStreamReader(new FileInputStream(file));
+            AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
+            c_ast_ascendente.ConstructorASTTiny asint_asc = new c_ast_ascendente.ConstructorASTTiny(alex);
+            Prog prog_asc = (Prog)asint_asc.parse().value;
+        } else if (tipo.equals("desc")) {
+            c_ast_descendente.ConstructorASTsTiny asint_desc = new c_ast_descendente.ConstructorASTsTiny(new FileReader(file));
+            asint_desc.disable_tracing();
+            Prog prog_desc = asint_desc.analiza();
+        } else {
+            System.err.println("Uso: java -jar Procesamientos.jar <fichero> <rec|inter|vis> <asc|desc>");
+            System.exit(1);
+        }
+
+        if(proc.equals("rec")) {
+            if(tipo.equals("asc")) {
+                System.out.println(new Procesamiento_rec().imprime(prog_asc));
+            }
+            else if (tipo.equals("desc")) {
+                System.out.println(new Procesamiento_rec().imprime(prog_desc));
+            }
+        } else if(proc.equals("inter") {
+            if(tipo.equals("asc")) {
+                prog_asc.imprime();
+            }
+            else if(tipo.equals("desc")) {
+                prog_desc.imprime();
+            }
+        }
     }
 }   
