a8413b2e4e085a5916ce029ef93ac1b599760525
Miguel Verdaguer Velázquez <verdx@riseup.net>
2024-04-10 13:09:55 +0200

    Espacios y tracing en javacc de fase2

--- asint/implementacion_javacc/asint/Launcher_javacc.java
+++ asint/implementacion_javacc/asint/Launcher_javacc.java
@@ -1,22 +1,23 @@
 package implementacion_javacc.asint;
 import java.io.FileReader;
 public class Launcher_javacc{
-   public static void launch(String file) throws Exception {
-      AnalizadorSintacticoTinyMain asint = new AnalizadorSintacticoTinyMain(new FileReader(file));
-      try{
-        asint.analiza();
-      } catch (Exception e) {
-        System.out.println(e.getMessage());
-        System.exit(1);
-      }
-      System.out.println("OK");
-   }
+    public static void launch(String file) throws Exception {
+        AnalizadorSintacticoTinyMain asint = new AnalizadorSintacticoTinyMain(new FileReader(file));
+        try{
+            asint.enable_tracing();
+            asint.analiza();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+        System.out.println("OK");
+    }
 
-   public static void main(String[] args) throws Exception {
-      if (args.length != 1) {
-         System.err.println("Uso: java Launcher_javacc <fichero>");
-         System.exit(1);
-      }
-      launch(args[0]);
-   }
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.err.println("Uso: java Launcher_javacc <fichero>");
+            System.exit(1);
+        }
+        launch(args[0]);
+    }
 }
