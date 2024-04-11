51235e91be7d31bb3c90005af4a4ff0982a84112
Miguel Verdaguer Velázquez <verdx@riseup.net>
2024-04-11 13:00:46 +0200

    Hacer que el ascendente imprima los tokens en el alex

--- fase3/Main.java
+++ fase3/Main.java
@@ -9,6 +9,49 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 
 public class Main {
+    public static void launch(String[] args) throws Exception {
+
+        char tipo;
+        Prog prog = null;
+
+        tipo = (char) System.in.read();
+        
+        //Inicializamos el arbol de sintaxis abstracta que vayamos a usar
+        if(tipo == 'a') {
+
+            System.out.prinln("CONSTRUCCION AST ASCENDENTE");
+
+            Reader input = new InputStreamReader(System.in);
+            AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
+            c_ast_ascendente.ConstructorASTTiny asint_asc = new c_ast_ascendente.ConstructorASTTiny(alex);
+            try {
+                prog = (Prog)asint_asc.debug_parse().value;
+            } catch (Exception|Error e) {
+                System.err.println(e.getMessage());
+                System.exit(0);
+            }
+        } else if (tipo == 'd') {
+            c_ast_descendente.ConstructorASTsTiny asint_desc = new c_ast_descendente.ConstructorASTsTiny(System.in);
+            asint_desc.disable_tracing();
+            try {
+                prog = asint_desc.analiza();
+            } catch (Exception|Error e) {
+                System.err.println(e.getMessage());
+                System.exit(0);
+            }
+        } else {
+            prog = null;
+        }
+        
+        System.out.println("IMPRESION RECURSIVA");
+        System.out.println(new Procesamiento_rec().imprime(prog));
+        System.out.println("IMPRESION INTERPRETE");
+        prog.imprime();
+        System.out.println("IMPRESION VISITANTE");
+        prog.procesa(new Procesamiento_vis());
+    }
+
+
     public static void main(String[] args) throws Exception {
         if(args.length != 3) {
             System.err.println("Uso: java -cp java-cup-11b-29160615-3.jar:. procesamientos.Main <fichero> <rec|inter|vis> <asc|desc>");
@@ -19,13 +62,13 @@ public class Main {
         String tipo = args[2];
 
         //Inicializamos el arbol de sintaxis abstracta que vayamos a usar
-        Prog prog_asc = null, prog_desc = null;
+        Prog prog = null;
         if(tipo.equals("asc")) {
             Reader input = new InputStreamReader(new FileInputStream(file));
             AnalizadorLexicoTiny alex = new AnalizadorLexicoTiny(input);
-            c_ast_ascendente.ConstructorASTTiny asint_asc = new c_ast_ascendente.ConstructorASTTiny(alex);
+            c_ast_ascendente.ConstructorASTTinyDJ asint_asc = new c_ast_ascendente.ConstructorASTTinyDJ(alex);
             try {
-                prog_asc = (Prog)asint_asc.parse().value;
+                prog = (Prog)asint_asc.debug_parse().value;
             } catch (Exception|Error e) {
                 System.err.println(e.getMessage());
                 System.exit(0);
@@ -34,7 +77,7 @@ public class Main {
             c_ast_descendente.ConstructorASTsTiny asint_desc = new c_ast_descendente.ConstructorASTsTiny(new FileReader(file));
             asint_desc.disable_tracing();
             try {
-                prog_desc = asint_desc.analiza();
+                prog = asint_desc.analiza();
             } catch (Exception|Error e) {
                 System.err.println(e.getMessage());
                 System.exit(0);
@@ -45,27 +88,11 @@ public class Main {
         }
 
         if(proc.equals("rec")) {
-            if(tipo.equals("asc")) {
-                System.out.println(new Procesamiento_rec().imprime(prog_asc));
-
-            }
-            else if (tipo.equals("desc")) {
-                System.out.println(new Procesamiento_rec().imprime(prog_desc));
-            }
+            //System.out.println(new Procesamiento_rec().imprime(prog));
         } else if(proc.equals("inter")) {
-            if(tipo.equals("asc")) {
-                prog_asc.imprime();
-            }
-            else if(tipo.equals("desc")) {
-                prog_desc.imprime();
-            }
+            //prog.imprime();
         } else if (proc.equals("vis")) {
-            if(tipo.equals("asc")) {
-                prog_asc.procesa(new Procesamiento_vis());
-            }
-            else if(tipo.equals("desc")) {
-                prog_desc.procesa(new Procesamiento_vis());
-            }
+            //prog.procesa(new Procesamiento_vis());
         } else {
             System.err.println("Uso: java -cp java-cup-11b-29160615-3.jar:. procesamientos.Main <fichero> <rec|inter|vis> <asc|desc>");
             System.exit(1);
