02c08236c3e56cdfa70db1970fdfb880c303b304
Miguel Verdaguer Velázquez <verdx@riseup.net>
2024-05-07 18:27:58 +0200

    Cambio del emit en la generacion de codigo

--- fase4/implementacion/procesamientos/GeneracionCod_vis.java
+++ fase4/implementacion/procesamientos/GeneracionCod_vis.java
@@ -7,151 +7,135 @@ import maquinap.MaquinaP.*;
 
 public class GeneracionCod_vis extends ProcesamientoDef {
     
-    private MaquinaP maquinaP;
+    private MaquinaP m;
 
     public GeneracionCod_vis() {
         this.maquinaP = new MaquinaP();
     }
 
-    public void emit(Instruccion ins) {
-        maquinaP.emit(ins);
-    }
-
-
     public void procesa(Asig exp) {
         exp.arg1().procesa(this);
         exp.arg2().procesa(this);
-        emit(new DesapilaDir(exp.arg1().vinculo().dir()));
+        //TODO
+    }
+
+    public void procesa(Indexacion exp) {
+        exp.opnd0().procesa(this);
+        exp.opnd1().procesa(this);
+        m.emit(m.apila_int(exp.opnd0().getTipo().tipo().getTam()));
+        m.emit(m.mul());
+        m.emit(m.suma());
+    }
+
+    public void procesa(Acceso exp) {
+        exp.opnd().procesa(this);
+        //TODO
     }
     
     public void procesa(Suma exp) {
         exp.arg1().procesa(this);
         exp.arg2().procesa(this);
-        if(exp instanceof Tipo_int) {
-            emit(new ISumaInt());
-        } else if (exp instanceof Tipo_real){
-            emit(new ISumaReal());
-        }
+        m.emit(m.suma());
     }
 
     public void procesa(Resta exp) {
         exp.arg1().procesa(this);
         exp.arg2().procesa(this);
-        if(exp instanceof Tipo_int) {
-            emit(new IRestaInt());
-        } else {
-            emit(new IRestaReal());
-        }
+        m.emit(m.resta());
     }
 
     public void procesa(Mul exp) {
         exp.arg1().procesa(this);
         exp.arg2().procesa(this);
-        if(exp instanceof Tipo_int) {
-            emit(new IMulInt());
-        } else {
-            emit(new IMulReal());
-        }
+        m.emit(m.mul());
     }
 
     public void procesa(Div exp) {
         exp.arg1().procesa(this);
         exp.arg2().procesa(this);
-        if(exp instanceof Tipo_int) {
-            emit(new IDivInt());
-        } else {
-            emit(new IDivReal());
-        }
+        m.emit(m.div());
     }
 
     public void procesa(And exp) {
         exp.arg1().procesa(this);
         exp.arg2().procesa(this);
-        emit(new IAnd());
+        m.emit(m.and());
     }
 
     public void procesa(Or exp) {
         exp.arg1().procesa(this);
         exp.arg2().procesa(this);
-        emit(new IOr());
+        m.emit(m.or());
     }
 
     public void procesa(Menor exp) {
         exp.arg1().procesa(this);
         exp.arg2().procesa(this);
-        if(exp instanceof Tipo_int) {
-            emit(new IMenor());
-        }
+        m.emit(m.menor());
     }
 
     public void procesa(Menor_igual exp) {
         exp.arg1().procesa(this);
         exp.arg2().procesa(this);
-        if(exp instanceof Tipo_int) {
-            emit(new IMenorIgual());
-        }
+        m.emit(m.menor_igual());
     }
 
     public void procesa(Mayor exp) {
         exp.arg1().procesa(this);
         exp.arg2().procesa(this);
-        if(exp instanceof Tipo_int) {
-            emit(new IMayor());
-        }
+        m.emit(m.mayor());
     }
 
     public void procesa(Mayor_igual exp) {
         exp.arg1().procesa(this);
         exp.arg2().procesa(this);
-        if(exp instanceof Tipo_int) {
-            emit(new IMayorIgual());
-        }
+        m.emit(m.mayor_igual());
     }
 
     public void procesa(Igual_comp exp) {
         exp.arg1().procesa(this);
         exp.arg2().procesa(this);
-        emit(new IIgualdad());
+        m.emit(m.igualdad());
     }
 
     public void procesa(Distinto_comp exp) {
         exp.arg1().procesa(this);
         exp.arg2().procesa(this);
-        emit(new IDesigualdad());
+        m.emit(m.desigualdad());
     }
 
     public void procesa(Menos_unario exp) {
         exp.arg().procesa(this);
-        emit(new IMenosUnario());
+        m.emit(m.menos_unario());
     }
 
     public void procesa(Not exp) {
         exp.arg().procesa(this);
-        emit(new INot());
+        m.emit(m.not());
     }
 
     public void procesa(Lit_ent exp) {
-        emit(new IApilaInt(exp.valor()));
+        m.emit(m.apila_int(exp.valor()));
     }
 
     public void procesa(Lit_real exp) {
-        emit(new IApilaReal(exp.valor()));
+        m.emit(m.apila_real(exp.valor()));
     }
 
     public void procesa(True exp) {
-        emit(new IApilaBool(true));
+        m.emit(m.apila_bool(true));
     }
 
     public void procesa(False exp) {
-        emit(new IApilaBool(false));
+        m.emit(m.apila_bool(false));
     }
 
     public void procesa(Lit_cadena exp) {
-        emit(new IApilaString(exp.valor()));
+        m.emit(m.apila_string(exp.valor()));
     }
 
     public void procesa(Null exp) {
-        emit(new IApilaNull());
+        m.emit(m.apila_null());
     }
 
 }
